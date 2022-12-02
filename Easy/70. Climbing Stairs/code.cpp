98f083cb9bf2641c6d98172b369469087d338337
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-12-02 18:45:42 +0530

    Create code.cpp

---
+++ Easy/70. Climbing Stairs/code.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=1)
+        {
+            return n;
+        }
+        int prev=1;
+        int curr=1;
+        for(int i=2;i<=n;i++)
+        {
+            int temp=prev+curr;
+            prev=curr;
+            curr=temp;
+        }
+        return curr;
+    }
+};
